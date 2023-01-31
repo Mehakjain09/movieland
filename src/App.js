7dd843872306689f11e065304898bf62800dfc39
Mehakjain09 <kalamehakjain@gmail.com>
2023-01-31 11:32:38 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MovieCard from "./MovieCard";
+import pic from "./search.svg";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [serachtext, setSearchtext] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchData = async (value) => {
+    const resp = await fetch(
+      "http://www.omdbapi.com?apikey=e4bded8b&s=" + `${value}`
+    );
+    const data = await resp.json();
+    console.log(data.Search);
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    fetchData("Movie");
+    setIsLoading(false);
+  }, []);
+
+  function handleClick() {
+    console.log(serachtext);
+    fetchData(serachtext);
+    console.log(movies);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>MOVIELAND</h1>
+      </div>
+      <div className="searchbar">
+        <div className="search">
+          <input
+            type="text"
+            value={serachtext}
+            onChange={(e) => setSearchtext(e.target.value)}
+          />
+        </div>
+        <button onClick={handleClick}>Search</button>
+        {
+          //<image src={pic} alt="search logo" onClick={handleClick}/>
+        }
+      </div>
+      <div className="movies">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          movies.map((movie, index) => <MovieCard key={index} movie={movie} />)
+        )}
+      </div>
     </div>
   );
 }
